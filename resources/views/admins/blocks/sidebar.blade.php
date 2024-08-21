b9806c283b3dda5f8dcc5164cf91560f811f31b9
nguyendinhduck03 <nguyendinhduc.yb.k03@gmail.com>
2024-08-21 12:39:56 +0700

    second commit

--- resources/views/admins/blocks/sidebar.blade.php
+++ resources/views/admins/blocks/sidebar.blade.php
@@ -17,20 +17,6 @@
             <a class="nav-link " href="{{route('comments.index')}}">Comment</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#" onclick="toggleSubmenu(event)">Statistical</a>
-            <ul class="nav flex-column submenu">
-                <li class="nav-item">
-                    <a class="nav-link" href="">View Product</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('dailyStats')}}">Statistics by day</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Statistics by month</a>
-                </li>
-            </ul>
-        </li>
-        <li class="nav-item">
             <a class="nav-link " href="{{route('orders.index')}}">Order</a>
         </li>
         
